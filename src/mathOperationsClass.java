48b2421ed2cd981434c0ba94804b36a592c640fe
Marcos <marcos.vinicius.dev.2004@gmail.com>
2023-01-25 21:32:22 -0300

    Simplificando métodos de cálculo de resistência equivalente

--- src/mathOperationsClass.java
+++ src/mathOperationsClass.java
@@ -1,14 +1,12 @@
 public class mathOperationsClass{
     
-    //Função para calcular a resistência equivalente na associação em paralelo de dois resistores.
-    public double equivalenciaParalelaDupla(double R1,  double R2){
-        return (R1 * R2) / (R1 + R2);
-    } 
-
     //Função para calcular a resistencia equivalente na associação em paralelo de resistores.
     public double equivalenciaParalela(double R1){
         return 1 / R1;
     }
 
     //Função para calcular a resistencia equivalente na associação em sequência de resistores.
+    public double equivalenciaSequencia(double R1){
+        return R1;
+    }
 }
