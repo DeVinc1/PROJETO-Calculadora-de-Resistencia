c736a226f4d1400b252a697f9136b26adc1e20ab
Marcos <marcos.vinicius.dev.2004@gmail.com>
2023-01-25 21:31:49 -0300

    Construindo lógica de funcionamento do sistema.

--- src/mainClass.java
+++ src/mainClass.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class mainClass {
     public static void main(String[] args) throws Exception {
+
+        mathOperationsClass operacoes = new mathOperationsClass();
+        Scanner input = new Scanner(System.in);
+
+        char continuar = 'z';
+        double resistenciaEquivalente = 0;
+
+        //Loop para calcular a resistencia equivalente de um circuito qualquer.
+        while(continuar != 'n'){
+
+            System.out.println("Qual a associação de resistores que será tratada \n" +  
+            "\t a. Associação de resistores em sequência." +
+            "\t b. Associação de resistores em paralelo.");
+        }
+        input.close();
 
     }
 }
